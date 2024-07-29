a94741302133d952a6c4c6bac907259aa8883747
durib <duri.bradshaw@gmail.com>
2024-07-29 22:28:13 +1000

    reorginised

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def index():
-    return send_file('static/leaflet/index.html')
+    return send_file('static/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
